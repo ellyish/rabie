98802c1934ee97f2bb3cb7534fd30b7ad41fcec7
Elly M. iShmael <iraqgeeks@gmail.com>
2014-07-23 06:35:50 +0300

    first commit

---
+++ page.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @package WordPress
+ * @subpackage HTML5_Boilerplate
+ */
+
+get_header(); ?>
+
+<div id="main" role="main">
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+  <article class="post" id="post-<?php the_ID(); ?>">
+  
+    <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+
+    <?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+  
+  </article>
+  <?php endwhile; endif; ?>
+  <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+
+
+</div>
+
+<!-- <?php get_sidebar(); ?>
+-->
+<?php get_footer(); ?>
